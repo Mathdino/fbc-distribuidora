35f6c2cb596b229702a835800913881d65b78807
Matheus Bernardino <mmtheus69@gamil.com>
2024-12-03 22:15:41 -0300

    finalizado o projeto fbc

---
+++ js/dados.js
@@ -0,0 +1,532 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "Coca_cola_2L",
+            "img": "./img/cardapio/refrigerante/cocacola_2l.webp",
+            "name": "Coca-cola 2L",
+            "dsc": "Coca-cola 2L",
+            "price": 8.29
+        },
+        {
+            "id": "Coca_cola_1-5L",
+            "img": "./img/cardapio/refrigerante/cocacola_1l500.webp",
+            "name": "Coca-cola 1,5L",
+            "dsc": "Coca-cola 1,5L",
+            "price":  7.99
+        },
+        {
+            "id": "Coca_cola_600ml",
+            "img": "./img/cardapio/refrigerante/cocacola_600ml.webp",
+            "name": "Coca-cola 600ml",
+            "dsc": "Coca-cola 600ml",
+            "price":  4.05
+        },
+        {
+            "id": "Coca_cola_350ml",
+            "img": "./img/cardapio/refrigerante/cocacola_350ml.webp",
+            "name": "Coca-cola 350ml",
+            "dsc": "Coca-cola 350ml",
+            "price": 2.99
+        },
+        {
+            "id": "Guaraviton_500ml",
+            "img": "./img/cardapio/refrigerante/guaraviton_500ml.avif",
+            "name": "Guaraviton 500ml",
+            "dsc": "Guaraviton 500ml",
+            "price": 1.99
+        }
+    ],
+    "burgers": [
+        {
+            "id": "ruffles_original_76",
+            "img": "./img/cardapio/salgadinho/ruffles_original.webp",
+            "name": "Ruffles Original 76g",
+            "dsc": "Ruffles original 76g",
+            "price": 6.99
+        },
+        {
+            "id": "ruffles_cebola_76",
+            "img": "./img/cardapio/salgadinho/ruffles_cebola.webp",
+            "name": "Ruffles Cebola 76g",
+            "dsc": "Ruffles cebola 76g",
+            "price": 6.99
+        },
+        {
+            "id": "ruffles_churrasco_76",
+            "img": "./img/cardapio/salgadinho/ruffles_churrasco.webp",
+            "name": "Ruffles Churrasco 76g",
+            "dsc": "Ruffles churrasco 76g",
+            "price": 6.99
+        },
+        {
+            "id": "ruffles_original_115",
+            "img": "./img/cardapio/salgadinho/ruffles_original115.webp",
+            "name": "Ruffles Original 115g",
+            "dsc": "Ruffles Original 115g",
+            "price": 10.49
+        },
+        {
+            "id": "Fandangos_presunto_105",
+            "img": "./img/cardapio/salgadinho/fandangos_presunto.webp",
+            "name": "Fandangos Presunto 105g",
+            "dsc": "Fandangos Presunto 105",
+            "price": 6.26
+        },
+        {
+            "id": "Fandangos_queijo_105",
+            "img": "./img/cardapio/salgadinho/fandangos_queijo.webp",
+            "name": "Fandangos Queijo 105g",
+            "dsc": "Fandangos Queijo 105",
+            "price": 6.26
+        },
+        {
+            "id": "Doritos_75",
+            "img": "./img/cardapio/salgadinho/doritos75.webp",
+            "name": "Doritos 75g",
+            "dsc": "Doritos 75g",
+            "price": 6.99
+        },
+        {
+            "id": "Doritos_120",
+            "img": "./img/cardapio/salgadinho/doritos120.webp",
+            "name": "Doritos 120g",
+            "dsc": "Doritos 120g",
+            "price": 10.49
+        },
+        {
+            "id": "Lays_original_70",
+            "img": "./img/cardapio/salgadinho/lays_original.webp",
+            "name": "Lays Original 70g",
+            "dsc": "Lays Original 70g",
+            "price": 6.99
+        },
+        {
+            "id": "Lays_Cebola_70",
+            "img": "./img/cardapio/salgadinho/lays_Cebola.webp",
+            "name": "Lays Cebola 70g",
+            "dsc": "Lays Cebola 70g",
+            "price": 6.99
+        },
+        {
+            "id": "Lays_sensacão_frango_grelhado_40",
+            "img": "./img/cardapio/salgadinho/lays_sensacao_frangogrelhado.png",
+            "name": "Sensacão Frango Grelhado 40g",
+            "dsc": "Sensacão Frango Grelhado 40g",
+            "price": 4.42
+        },
+        {
+            "id": "Lays_sensacão_peitodeperu_40",
+            "img": "./img/cardapio/salgadinho/lays_sensacao_peitodeperu.webp",
+            "name": "Sensacão Peito de Peru 40g",
+            "dsc": "Sensacão Peito de Peru 40g",
+            "price": 4.42
+        },
+        {
+            "id": "Lays_original_35",
+            "img": "./img/cardapio/salgadinho/lays_original35.webp",
+            "name": "Lays Original 35g",
+            "dsc": "Lays Original 35g",
+            "price": 3.62
+        },
+        {
+            "id": "Lays_Cebola_35",
+            "img": "./img/cardapio/salgadinho/lays_Cebola35.jpg",
+            "name": "Lays Cebola 35g",
+            "dsc": "Lays Cebola 35g",
+            "price": 3.62
+        },
+        {
+            "id": "Cheetos_requeijão_105",
+            "img": "./img/cardapio/salgadinho/Cheetos_Requeijao.webp",
+            "name": "Cheetos Requeijão 105g",
+            "dsc": "Cheetos Requeijão 105g",
+            "price": 6.26
+        },
+        {
+            "id": "Cheetos_Lua_95",
+            "img": "./img/cardapio/salgadinho/Cheetos_Lua.webp",
+            "name": "Cheetos Lua 95g",
+            "dsc": "Cheetos Lua 95g",
+            "price": 6.26
+        },
+        {
+            "id": "Cheetos_Mix_82",
+            "img": "./img/cardapio/salgadinho/Cheetos_Mix.jpg",
+            "name": "Cheetos Mix 82g",
+            "dsc": "Cheetos Mix 82g",
+            "price": 6.26
+        },
+        {
+            "id": "Cebolitos_91",
+            "img": "./img/cardapio/salgadinho/Cebolitos91.webp",
+            "name": "Cebolitos 91g",
+            "dsc": "Cebolitos 91g",
+            "price": 6.99
+        },
+        {
+            "id": "Cebolitos_34",
+            "img": "./img/cardapio/salgadinho/Cebolitos34.jpg",
+            "name": "Cebolitos 34g",
+            "dsc": "Cebolitos 34g",
+            "price": 2.92
+        },
+        {
+            "id": "Baconzitos_86",
+            "img": "./img/cardapio/salgadinho/Baconzitos86.webp",
+            "name": "Baconzitos 86g",
+            "dsc": "Baconzitos 86g",
+            "price": 6.99
+        },
+        {
+            "id": "Baconzitos_34",
+            "img": "./img/cardapio/salgadinho/Baconzitos34.webp",
+            "name": "Baconzitos 34g",
+            "dsc": "Baconzitos 34g",
+            "price": 2.92
+        },
+        {
+            "id": "Amendoim_japonês_400",
+            "img": "./img/cardapio/salgadinho/Amendoin_Japones.webp",
+            "name": "Amendoim Japonês 400g",
+            "dsc": "Amendoim Japonês 400g",
+            "price": 5.99
+        },
+        {
+            "id": "Amendoim_semPele_100",
+            "img": "./img/cardapio/salgadinho/Amendoin_sempele.webp",
+            "name": "Amendoim sem Pele 100g",
+            "dsc": "Amendoim sem Pele 100g",
+            "price": 5.99
+        },
+        {
+            "id": "Ovinho_145",
+            "img": "./img/cardapio/salgadinho/ovinho.webp",
+            "name": "Ovinho 145g",
+            "dsc": "Ovinho 145g",
+            "price": 5.99
+        },
+        {
+            "id": "Pringles_Original_104",
+            "img": "./img/cardapio/salgadinho/Pringes_Original.jpg",
+            "name": "Pringles Original 104g",
+            "dsc": "Pringles Original 104g",
+            "price": 8.49
+        },
+        {
+            "id": "Pringles_Cebola_104",
+            "img": "./img/cardapio/salgadinho/Pringes_Cebola.jpg",
+            "name": "Pringles Cebola 104g",
+            "dsc": "Pringles Cebola 104g",
+            "price": 8.49
+        },
+        {
+            "id": "Pringles_Queijo_104",
+            "img": "./img/cardapio/salgadinho/Pringes_Queijo.jpg",
+            "name": "Pringles Queijo 104g",
+            "dsc": "Pringles Queijo 104g",
+            "price": 8.49
+        },
+        {
+            "id": "Pringles_Churrasco_104",
+            "img": "./img/cardapio/salgadinho/Pringes_Churrasco.jpg",
+            "name": "Pringles Churrasco 104g",
+            "dsc": "Pringles Churrasco 104g",
+            "price": 8.49
+        },
+        {
+            "id": "Pringles_Cheedar_e_Bacon_104",
+            "img": "./img/cardapio/salgadinho/Pringes_CheedarBacon.jpg",
+            "name": "Pringles Cheedar e Bacon 104g",
+            "dsc": "Pringles Cheedar e Bacon 104g",
+            "price": 8.49
+        },
+        {
+            "id": "Torcida_Churrasco_60",
+            "img": "./img/cardapio/salgadinho/torcida_churrasco60.webp",
+            "name": "Torcida Churrasco 60g",
+            "dsc": "Torcida Churrasco 60g",
+            "price": 1.85
+        },
+        {
+            "id": "Torcida_PimentaMexicana_60",
+            "img": "./img/cardapio/salgadinho/torcida_Pimenta_Mexicana60.webp",
+            "name": "Torcida Pimenta Mexicana 60g",
+            "dsc": "Torcida Pimenta Mexicana 60g",
+            "price": 1.85
+        },
+        {
+            "id": "Torcida_Cebola_60",
+            "img": "./img/cardapio/salgadinho/torcida_cebola60.webp",
+            "name": "Torcida Cebola 60g",
+            "dsc": "Torcida Cebola 60g",
+            "price": 1.85
+        },
+        {
+            "id": "Torcida_Queijo_60",
+            "img": "./img/cardapio/salgadinho/torcida_Queijo60.webp",
+            "name": "Torcida Queijo 60g",
+            "dsc": "Torcida Queijo 60g",
+            "price": 1.85
+        },
+        {
+            "id": "Torcida_CamarãocomPimenta_60",
+            "img": "./img/cardapio/salgadinho/torcida_Camarrao60.jpg",
+            "name": "Torcida Camarão com Pimenta 60g",
+            "dsc": "Torcida Camarão com Pimenta 60g",
+            "price": 1.85
+        },
+        {
+            "id": "Torcida_CostelinhacomLimão_60",
+            "img": "./img/cardapio/salgadinho/torcida_costelinha_limao60.webp",
+            "name": "Torcida Costelinha com Limão 60g",
+            "dsc": "Torcida Costelinha com Limão 60g",
+            "price": 1.85
+        },
+        {
+            "id": "Torcida_PãodeAlho_60",
+            "img": "./img/cardapio/salgadinho/torcida_Pao_de_Alho60.png",
+            "name": "Torcida Pão de Alho 60g",
+            "dsc": "Torcida Pão de Alho 60g",
+            "price": 1.85
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "Miojo_Cup_Noodles_GalinhaCaipira_69g",
+            "img": "./img/cardapio/diversos/miojo_cup_noodles69g.webp",
+            "name": "Miojo Cup Noodles Galinha Caipira 69g",
+            "dsc": "Miojo Cup Noodles Galinha Caipira 69g",
+            "price": 3.48
+        },
+        {
+            "id": "Miojo_Nissin_GalinhaCaipira_85g",
+            "img": "./img/cardapio/diversos/miojo_nissin85g.webp",
+            "name": "Miojo Nissin Galinha Caipira 85g",
+            "dsc": "Miojo Nissin Galinha Caipira 85g",
+            "price": 1.85
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "Cookies_Nestle_40g",
+            "img": "./img/cardapio/bolachas/cookies_nestle_60g.webp",
+            "name": "Cookies Nestlé 40g",
+            "dsc": "Cookies Nestlé 40g",
+            "price": 1.99
+        },
+        {
+            "id": "Wafer_chocolate_bauducco_140g",
+            "img": "./img/cardapio/bolachas/bauducco_wafer_chocolate140g.webp",
+            "name": "Wafer Chocolate Bauducco 140g",
+            "dsc": "Wafer Chocolate Bauducco 140g",
+            "price": 2.79
+        },
+        {
+            "id": "Wafer_Morango_bauducco_140g",
+            "img": "./img/cardapio/bolachas/bauducco_wafer_Morango140g.webp",
+            "name": "Wafer Morango Bauducco 140g",
+            "dsc": "Wafer Morango Bauducco 140g",
+            "price": 2.79
+        },
+        {
+            "id": "Recheado_Bauducco_108g",
+            "img": "./img/cardapio/bolachas/bauducco_recheado108g.webp",
+            "name": "Recheado Bauducco 108g",
+            "dsc": "Recheado Bauducco 108g",
+            "price": 1.99
+        },
+        {
+            "id": "Wafer_Chocolate_Visconti_120g",
+            "img": "./img/cardapio/bolachas/viscont_wafer120g.webp",
+            "name": "Wafer Chocolate Visconti 120g",
+            "dsc": "Wafer Chocolate Visconti 120g",
+            "price": 2.10
+        },
+        {
+            "id": "Wafer_Morango_Visconti_120g",
+            "img": "./img/cardapio/bolachas/viscont_wafer_morango120g.webp",
+            "name": "Wafer Morango Visconti 120g",
+            "dsc": "Wafer MorangoVisconti 120g",
+            "price": 2.10
+        },
+        {
+            "id": "Recheado_Chocolate_Visconti_125g",
+            "img": "./img/cardapio/bolachas/viscont_recheado_chocolate125g.webp",
+            "name": "Recheado Chocolate Visconti 125g",
+            "dsc": "Recheado Chocolate Visconti 125g",
+            "price": 2.85
+        },
+        {
+            "id": "Recheado_Morango_Visconti_125g",
+            "img": "./img/cardapio/bolachas/viscont_recheado_Morango125g.webp",
+            "name": "Recheado Morango Visconti 125g",
+            "dsc": "Recheado Morango Visconti 125g",
+            "price": 2.85
+        },
+        {
+            "id": "ÁguaeSal_Triunfo_164g",
+            "img": "./img/cardapio/bolachas/triunfo_aguaesal_164g.webp",
+            "name": "Água e Sal Triunfo 164g",
+            "dsc": "Água e Sal Triunfo 164g",
+            "price": 1.95
+        },
+        {
+            "id": "CreamCracker_Triunfo_164g",
+            "img": "./img/cardapio/bolachas/triunfo_cream_cracker_164g.webp",
+            "name": "Cream Cracker Triunfo 164g",
+            "dsc": "Cream Cracker Triunfo 164g",
+            "price": 1.95
+        },
+        {
+            "id": "Maizena_Triunfo_170g",
+            "img": "./img/cardapio/bolachas/triunfo_maizena_170g.webp",
+            "name": "Maizena Triunfo 170g",
+            "dsc": "Maizena Triunfo 170g",
+            "price": 1.95
+        }
+    ],
+    "pizzas": [
+        {
+            "id": "Energético_RedBull",
+            "img": "./img/cardapio/energeticos/redbull.webp",
+            "name": "RedBull",
+            "dsc": "Energético RedBull",
+            "price": 7.66
+        },
+        {
+            "id": "Energético_RedBull_Sem_Açucar",
+            "img": "./img/cardapio/energeticos/redbull_semacucar.jpg",
+            "name": "RedBull Sem Açucar",
+            "dsc": "Energético RedBull Sem Açucar",
+            "price": 7.66
+        },
+        {
+            "id": "Monster_Juice_MangoLoco_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Juice_MangoLoco_473ml.webp",
+            "name": "Monster Juice Mango Loco 473ml",
+            "dsc": "Monster Juice Mango Loco 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Original_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Original_473ml.webp",
+            "name": "Monster Original 473ml",
+            "dsc": "Monster Original 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Absolutely_Zero_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Absolutely_Zero_473ml.webp",
+            "name": "Monster Absolutely Zero 473ml",
+            "dsc": "Monster Absolutely Zero 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Ultra_Violet_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Ultra_Violet_473ml.webp",
+            "name": "Monster Ultra Violet 473ml",
+            "dsc": "Monster Ultra Violet 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Pipeline_Punch_437ml",
+            "img": "./img/cardapio/energeticos/Monster_Pipeline_Punch_437ml.webp",
+            "name": "Monster Pipeline Punch 437ml",
+            "dsc": "Monster Pipeline Punch 437ml",
+            "price": 7.15
+        },
+        {
+           "id": "Monster_Zero_Ultra_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Zero_Ultra_473ml.webp",
+            "name": "Monster Zero Ultra 473ml",
+            "dsc": "Monster Zero Ultra 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Ultra_Watermelon_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Ultra_Watermelon_473ml.webp",
+            "name": "Monster Ultra Watermelon 473ml",
+            "dsc": "Monster Ultra Watermelon 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Ultra_Paradise_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Ultra_Paradise_473ml.webp",
+            "name": "Monster Ultra Paradise 473ml",
+            "dsc": "Monster Ultra Paradise 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Juice_Khaos_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Juice_Khaos_473ml.png",
+            "name": "Monster Juice Khaos 473ml",
+            "dsc": "Monster Juice Khaos 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Dragon_IceTeaLimão_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Dragon_Ice_TeaLimão_473ml.webp",
+            "name": "Monster Dragon Ice Tea Limão 473ml",
+            "dsc": "Monster Dragon Ice Tea Limão 473ml",
+            "price": 7.15
+        },
+        {
+            "id": "Monster_Dragon_IceTeaPêssego_473ml",
+            "img": "./img/cardapio/energeticos/Monster_Dragon_Ice_TeaPêssego_473ml.webp",
+            "name": "Monster Dragon Ice Tea Pêssego 473ml",
+            "dsc": "Monster Dragon Ice Tea Pêssego 473ml",
+            "price": 7.15
+        }
+    ],
+    "steaks": [
+        {
+            "id": "Sonho_de_Valsa_1Kg",
+            "img": "./img/cardapio/doces/sonho_valsa1kg.webp",
+            "name": "Sonho de Valsa 1Kg",
+            "dsc": "Sonho de Valsa 1Kg",
+            "price": 45.55
+        },
+        {
+            "id": "Ouro_Branco_1Kg",
+            "img": "./img/cardapio/doces/ouro_branco1kg.webp",
+            "name": "Ouro Branco 1Kg",
+            "dsc": "Ouro Branco 1Kg",
+            "price": 45.55
+        },
+        {
+            "id": "Bala_7Belo_500g",
+            "img": "./img/cardapio/doces/bala_7belo500g.webp",
+            "name": "Bala 7Belo 500g",
+            "dsc": "Bala 7Belo 500g",
+            "price": 8.49
+        },
+        {
+            "id": "Bala_Poosh_500g",
+            "img": "./img/cardapio/doces/bala_poosh500g.webp",
+            "name": "Bala Poosh 500g",
+            "dsc": "Bala Poosh 500g",
+            "price": 8.49
+        },
+        {
+            "id": "Pirulito_Bruxilito_500g",
+            "img": "./img/cardapio/doces/pirulito_bruxilito500g.png",
+            "name": "Pirulito Bruxilito 500g",
+            "dsc": "Pirulito Bruxilito 500g",
+            "price": 10.89
+        },
+        {
+            "id": "Kinder_Ovo_c12",
+            "img": "./img/cardapio/doces/kinder_ovocx12.webp",
+            "name": "Kinder Ovo c/12",
+            "dsc": "Kinder Ovo c/12",
+            "price": 77.89
+        },
+        {
+           "id": "Mentos_c16",
+            "img": "./img/cardapio/doces/mentos_cx16.webp",
+            "name": "Mentos c/16",
+            "dsc": "Mentos c/16",
+            "price": 27.05
+        }
+    ]
+}
